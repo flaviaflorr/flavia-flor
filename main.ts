d35fa2edf0e1333250095bdeca43effb5d9987cc
flaviaflorr <114154662+flaviaflorr@users.noreply.github.com>
2022-10-06 08:49:13 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,44 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("prepare-se")
+    basic.pause(randint(1000, 5000))
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.clearScreen()
+})
+input.onPinPressed(TouchPin.P2, function () {
+    if (tocar == 0) {
+        cont2 += 1
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+input.onPinPressed(TouchPin.P1, function () {
+    if (tocar == 0) {
+        cont1 += 1
+        basic.showLeds(`
+            . . # . .
+            . # # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            `)
+        basic.pause(1000)
+        basic.clearScreen()
+    }
+})
+let tocar = 0
+let cont1 = 0
+let cont2 = 0
 basic.forever(function () {
 	
 })
